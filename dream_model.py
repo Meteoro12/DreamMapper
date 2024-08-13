95e350edd7ea341f9221af95e9f62ef5787875fa
Meteoro12 <91855995+Meteoro12@users.noreply.github.com>
2024-08-13 14:07:36 +0300

    Create file dream_model.py

---
+++ dream_model.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = SQLAlchemy()
+
+class Dream(db.Model):
+    __tablename__ = 'dreams'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, title, content, user_id):
+        self.title = title
+        self.content = content
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f"Dream('{self.title}', '{self.date}')"
+
+def initialize_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
