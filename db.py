52a8927f1750149786cbdcdcf27092eae1e567b4
Meteoro12 <91855995+Meteoro12@users.noreply.github.com>
2024-08-25 02:34:59 +0300

    cleaning code

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,11 +11,16 @@ DATABASE_HOST = os.getenv('DATABASE_HOST')
 DATABASE_NAME = os.getenv('DATABASE_NAME')
 DATABASE_PORT = os.getenv('DATABASE_PORT', '5432')
 
-connection_string = f'postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
+connection_string = (
+    f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}"
+    f"@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+)
 
 engine = create_engine(connection_string)
 
-SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+SessionLocal = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine)
+)
 
 def get_db_connection():
     db = SessionLocal()
@@ -23,3 +28,6 @@ def get_db_connection():
         yield db
     finally:
         db.close()
+
+if __name__ == "__main__":
+    pass
